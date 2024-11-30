69c21666041a89cd3e3102f57f29f5906fc56a8d
Tom Lynch <twhlynch@gmail.com>
2024-11-30 20:20:42 +1100

    start with parellel routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
@@ -11,13 +12,15 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
         <div key={image.id} className="flex flex-col w-48 h-48">
-          <Image
-            src={image.url}
-            style={{ objectFit: "contain" }}
-            width={480}
-            height={480}
-            alt={image.name}
-          />
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              style={{ objectFit: "contain" }}
+              width={480}
+              height={480}
+              alt={image.name}
+            />
+          </Link>
           <div>{image.name}</div>
         </div>
       ))}
