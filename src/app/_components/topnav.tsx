797f8fc1f8fab4464ccfeca405295856f819cf46
Tom Lynch <twhlynch@gmail.com>
2024-12-03 06:52:32 +1100

    nav links to home

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,10 +1,11 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { SimpleUploadButton } from "./simple-upload-button";
+import Link from "next/link";
 
 export function TopNav() {
     return (
       <nav className="flex items-center justify-between p-4 text-xl font-semibold border-b">
-        <div>Gallery</div>
+        <Link href="/">Gallery</Link>
         
         <div className="flex flex-row gap-4 items-center">
             <SignedOut>
